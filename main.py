d997a5bce2b74ffd6cdeb4707f8b92fbf2f62be4
blazern <danil.garmanov@protonmail.com>
2022-07-23 19:03:15 +0300

    Add nouns

--- main.py
+++ main.py
@@ -1,4 +1,76 @@
 import sys
 
+nounsMale = {
+  'Apfel',
+  'Arzt',
+  'Beruf',
+  'Brief',
+  'Bus',
+  'Bruder',
+  'Garten',
+  'Kuchen',
+  'Mund',
+  'Regen',
+  'Saft',
+  'Tag',
+  'Tisch',
+  'Vater',
+  'Zug',
+}
+
+nounsFemale = {
+  'Bahn',
+  'Firma',
+  'Arbeit',
+  'Küche',
+  'Tasche',
+  'Tomate',
+  'Toilette',
+  'Woche',
+  'Zeitung',
+  'Bank',
+  'Banane',
+  'Birne',
+  'Blume',
+  'Dame',
+  'Flasche',
+}
+
+nounsNeuter = {
+  'Foto',
+  'Essen',
+  'Dorf',
+  'Datum',
+  'Buch',
+  'Bild',
+  'Bad',
+  'Auto',
+  'Lied',
+  'Gepäck',
+  'Glas',
+  'Glück',
+  'Haus',
+  'Hotel',
+  'Konto',
+}
+
+nounsPlural = {
+  'Äpfel',
+  'Berufe',
+  'Briefe',
+  'Busse',
+  'Brüder',
+  'Arbeiten',
+  'Taschen',
+  'Tomaten',
+  'Toiletten',
+  'Bananen',
+  'Fotos',
+  'Dörfer',
+  'Bücher',
+  'Bilder',
+  'Lieder',
+}
+
 if __name__ == '__main__':
   sys.exit(main(sys.argv[1:]))
