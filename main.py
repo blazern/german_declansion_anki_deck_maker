ead02b5f59aa1a8d0fe2758fd2157905780f5a78
blazern <danil.garmanov@protonmail.com>
2022-07-23 20:19:07 +0300

    Strong declansion, nominative

--- main.py
+++ main.py
@@ -1,6 +1,38 @@
 import sys
+from enum import Enum
 
-nounsMale = {
+class Declansion(Enum):
+  STRONG = 1
+  WEAK = 2
+  MIXED = 3
+
+class Gener(Enum):
+  MALE = 1
+  FEMALE = 2
+  NEUTER = 3
+  PLURAL = 4
+
+demonstrative_pronouns = {
+  'dies',
+}
+
+indefinite_articles = {
+  'ein',
+  'kein',
+}
+
+possessive_pronouns = {
+  'mein',
+  'dein',
+  'sein',
+  'Ihr',
+  'sein',
+  'unser',
+  'eur',
+  'ihr',
+}
+
+nouns_male = {
   'Apfel',
   'Arzt',
   'Beruf',
@@ -18,7 +50,7 @@ nounsMale = {
   'Zug',
 }
 
-nounsFemale = {
+nouns_female = {
   'Bahn',
   'Firma',
   'Arbeit',
@@ -36,7 +68,7 @@ nounsFemale = {
   'Flasche',
 }
 
-nounsNeuter = {
+nouns_neuter = {
   'Foto',
   'Essen',
   'Dorf',
@@ -54,7 +86,7 @@ nounsNeuter = {
   'Konto',
 }
 
-nounsPlural = {
+nouns_plural = {
   'Äpfel',
   'Berufe',
   'Briefe',
@@ -82,6 +114,114 @@ adjectives = {
   'kalt',
   'heiß',
 }
+
+translations = {
+  'dies': 'this',
+  'ein': 'a',
+  'kein': 'no',
+  'mein': 'my',
+  'dein': 'your (informal)',
+  'sein': 'his',
+  'Ihr': 'your (formal)',
+  'sein': 'his',
+  'unser': 'our',
+  'eur': 'your (plural informal)',
+  'Apfel': 'apple',
+  'Arzt': 'doctor',
+  'Beruf': 'profession',
+  'Brief': 'letter',
+  'Bus': 'bus',
+  'Bruder': 'brother',
+  'Garten': 'garden',
+  'Kuchen': 'pie',
+  'Mund': 'mouth',
+  'Regen': 'rain',
+  'Saft': 'juice',
+  'Tag': 'day',
+  'Tisch': 'table',
+  'Vater': 'father',
+  'Zug': 'train',
+  'Bahn': 'subway train',
+  'Firma': 'firm',
+  'Arbeit': 'job',
+  'Küche': 'kitchen',
+  'Tasche': 'bag',
+  'Tomate': 'tomato',
+  'Toilette': 'toilet',
+  'Woche': 'week',
+  'Zeitung': 'newspaper',
+  'Bank': 'bank',
+  'Banane': 'banana',
+  'Birne': 'pear',
+  'Blume': 'flower',
+  'Dame': 'lady',
+  'Flasche': 'bottle',
+  'Foto': 'photo',
+  'Essen': 'food',
+  'Dorf': 'village',
+  'Datum': 'date',
+  'Buch': 'book',
+  'Bild': 'picture',
+  'Bad': 'bath',
+  'Auto': 'car',
+  'Lied': 'song',
+  'Gepäck': 'luggage',
+  'Glas': 'glass',
+  'Glück': 'luck',
+  'Haus': 'house',
+  'Hotel': 'hotel',
+  'Konto': 'account',
+  'Äpfel': 'apples',
+  'Berufe': 'professions',
+  'Briefe': 'letters',
+  'Busse': 'busses',
+  'Brüder': 'brothers',
+  'Arbeiten': 'jobs',
+  'Taschen': 'bags',
+  'Tomaten': 'tomatoes',
+  'Toiletten': 'toilets',
+  'Bananen': 'bananas',
+  'Fotos': 'photos',
+  'Dörfer': 'villages',
+  'Bücher': 'books',
+  'Bilder': 'pictures',
+  'Lieder': 'songs',
+  'groß': 'big',
+  'klein': 'small',
+  'lang': 'long',
+  'kurz': 'short',
+  'gut': 'good',
+  'schlecht': 'bad',
+  'kalt': 'cold',
+  'heiß': 'hot',
+}
+
+def main(args):
+  result = []
+  # Strong declansion
+  # Nominativ
+  # Male
+  for noun in nouns_male:
+    for adjective in adjectives:
+      german = '{}er {}'.format(adjective, noun)
+      english = '{} {}'.format(translations[adjective], translations[noun])
+      result.append('{}: \t{}'.format(german, english))
+  # Female
+  for noun in nouns_female:
+    for adjective in adjectives:
+      german = '{}e {}'.format(adjective, noun)
+      english = '{} {}'.format(translations[adjective], translations[noun])
+      result.append('{}: \t{}'.format(german, english))
+  # Neuter
+  for noun in nouns_neuter:
+    for adjective in adjectives:
+      german = '{}es {}'.format(adjective, noun)
+      english = '{} {}'.format(translations[adjective], translations[noun])
+      result.append('{}: \t{}'.format(german, english))
+
+  for entry in result:
+    print(entry)
+
 
 if __name__ == '__main__':
   sys.exit(main(sys.argv[1:]))
